052899d0154cb7b70d5b4c9ef582e8e715986d36
Randall Naar <rnd.naar@gmail.com>
2025-10-17 18:40:05 -0400

    Small cleanup.

--- modify-tx-with-rangeproof.ts
+++ modify-tx-with-rangeproof.ts
@@ -9,295 +9,167 @@ import { txhex4 } from "./txhex4";
 import { invert } from "@noble/curves/abstract/modular";
 import { generateRangeProof2 } from "./play-rewind";
 import { txhex5 } from "./txhex5";
+import { txhex6 } from "./tx6";
 
 function copyBytes(b: Buffer) {
   return Buffer.from(b.toString("hex"), "hex");
 }
 
-let t = liquid.Transaction.fromHex(txhex5);
-
-//reset
-const index = 1;
-
-// Subtracting commt to make pubkeys isnt working?
-
-// console.log("\n\nTXHEX1\n\n", t.toHex());
-
-function convertParityByteToQuadness(bytes: Uint8Array) {
-  if (bytes.length === 0) {
-    throw new Error("Bytes cannot be zero-length");
+function decryptByteValues(
+  signatures: Uint8Array,
+  decryptionKeys: Uint8Array[]
+): { decryptedByteValues: number[] } {
+  let decryptedByteValues: number[] = [];
+  for (let i = 0; i < 3328; i++) {
+    let byteNum = i;
+    let row = Math.floor(byteNum / 32);
+    let index = byteNum % 32;
+    decryptedByteValues.push(signatures[i] ^ decryptionKeys[row][index]);
   }
 
-  /* I've seen prefix values of 9, 10, and 11. 
-  I don't know what format these public keys have been serialized in.
-  I've seen a 9 as 1, a 10 as 0, and an 11 as 1.
-  Maybe oddness indicates quadness of y?
-  */
+  let decryptedByteValuesBuffer = Buffer.from(decryptedByteValues);
 
-  // if(bytes[0] >= 10) {
-  //   bytes[0] = 0;
-  // }
-  // else {
-  //   bytes[0] = 1;
-  // }
+  console.log("DECRYPTED", decryptedByteValuesBuffer.toString("hex"));
 
-  return (bytes[0] %= 2);
+  return { decryptedByteValues };
 }
 
-//el1qqwm6xvtm5467pw5xx6yqm4nl0rjhh3etaq0dhy8det0ufvfg607payculjrnyujv8h2nzmpw26vamg2zkre8yq8xqv0ez3yp8
-let blindingKey =
-  0x16d0a5cf8a1d8e9c242345a8f036fcc91cb231c043296a845e774ca4dd2bb2ecn;
-// let blindingKey = 0xabf582994e1518e46672669d44af3a522fd3c880e094f06fc12a7228549adab4n
-let nonceCommitment = t.outs[index].nonce.toString("hex");
-
-// let blinding = Fn.create(blindingKey)
-const ecdhNoncePreimage = sha256(
-  secp256k1.Point.fromHex(nonceCommitment).multiply(blindingKey).toBytes()
-);
-
-let nonce = BigInt(
-  "0x" + Buffer.from(sha256(ecdhNoncePreimage)).toString("hex")
-);
-// let nonce = BigInt("0x3009f2d6fbd5a965c0356987c9de180303b6e45839e90733e87806fa0f383739");
-// let nonce = BigInt("0x1a7e871335bc3c278c5f9f2231689d2d52e5af29a254a40beac6516420d79f3b");
-// let serializedPoint = t.outs[index].value.slice();
-let serializedPoint = copyBytes(t.outs[index].value);
-convertParityByteToQuadness(serializedPoint);
-
-let serializedGenP = copyBytes(t.outs[index].asset);
-convertParityByteToQuadness(serializedGenP);
-// serializedPoint[0] = 1;
-// serializedGenP[0] = 0;
-
-let genP = lift_x(Fn.fromBytes(serializedGenP.subarray(1)));
-
-// genP
-let genPXArr = [
-  3698275472572113n,
-  960805623148293n,
-  1361606309141106n,
-  2269370551941167n,
-  95069322382348n,
-];
-
-let genPYArr = [
-  90476339017378n,
-  3910324583965771n,
-  966802187916966n,
-  1105471236702902n,
-  117776083589745n,
-];
-
-let genPX = Fn.create(arrToPoint(genPXArr));
-let genPY = Fp.create(arrToPoint(genPYArr));
-let genP2 = new schnorr.Point(genPX, genPY, Fp.ONE);
-
-if (getQuadness(genP) != serializedGenP[0]) {
-  genP = genP.negate();
+//TODO: Take in an spk and a tx, then returns indexes of outputs on the tx that match
+function getOutputIndexes() {
+  return [1];
 }
 
-let decryptionKeys = genrand(
-  nonce,
-  serializedPoint,
-  serializedGenP,
-  new Uint8Array(Array(3328).fill(0))
-);
+function main() {
+  let t = liquid.Transaction.fromHex(txhex6);
 
-let signatures = Buffer.from(
-  t.outs[index].rangeProof!.subarray(846).toString("hex"),
-  "hex"
-);
+  //reset
+  const index = getOutputIndexes();
 
-let commitments = Buffer.from(
-  t.outs[index].rangeProof!.subarray(14, 814).toString("hex"),
-  "hex"
-);
+  function convertParityByteToQuadness(bytes: Uint8Array) {
+    if (bytes.length === 0) {
+      throw new Error("Bytes cannot be zero-length");
+    }
 
-let sharedRootMessageHash = Buffer.from(
-  t.outs[index].rangeProof!.subarray(814, 814 + 32).toString("hex"),
-  "hex"
-);
+    return (bytes[0] %= 2);
+  }
 
-// console.log(t)
+  let blindingKey =
+    0x16d0a5cf8a1d8e9c242345a8f036fcc91cb231c043296a845e774ca4dd2bb2ecn;
 
-/*
-base offset - 846
+  let nonceCommitment = t.outs[index].nonce.toString("hex");
 
-3320 bytes for second offset
+  const ecdhNoncePreimage = sha256(
+    secp256k1.Point.fromHex(nonceCommitment).multiply(blindingKey).toBytes()
+  );
 
-now we get row as /32*4
-/32 for index
+  let nonce = BigInt(
+    "0x" + Buffer.from(sha256(ecdhNoncePreimage)).toString("hex")
+  );
 
-*/
+  let serializedPoint = copyBytes(t.outs[index].value);
+  convertParityByteToQuadness(serializedPoint);
 
-let decryptedByteValues: number[] = [];
-for (let i = 0; i < 3328; i++) {
-  let byteNum = i;
-  let row = Math.floor(byteNum / 32);
-  let index = byteNum % 32;
-  decryptedByteValues.push(signatures[i] ^ decryptionKeys[row][index]);
+  let serializedGenP = copyBytes(t.outs[index].asset);
+  convertParityByteToQuadness(serializedGenP);
+
+  let genP = lift_x(Fn.fromBytes(serializedGenP.subarray(1)));
+  if (getQuadness(genP) != serializedGenP[0]) {
+    genP = genP.negate();
+  }
+
+  let decryptionKeys: Uint8Array[] = genrand(
+    nonce,
+    serializedPoint,
+    serializedGenP,
+    new Uint8Array(Array(3328).fill(0))
+  );
+
+  let signatures = Buffer.from(
+    t.outs[index].rangeProof!.subarray(846).toString("hex"),
+    "hex"
+  );
+
+  let commitments = Buffer.from(
+    t.outs[index].rangeProof!.subarray(14, 814).toString("hex"),
+    "hex"
+  );
+
+  let sharedRootMessageHash = Buffer.from(
+    t.outs[index].rangeProof!.subarray(814, 814 + 32).toString("hex"),
+    "hex"
+  );
+
+  let { decryptedByteValues } = decryptByteValues(signatures, decryptionKeys);
+
+  let valueBigIntArg: bigint = BigInt(
+    "0x" +
+      Buffer.from(
+        decryptedByteValues.slice(decryptedByteValues.length - 8)
+      ).toString("hex")
+  );
+
+  let decryptedByteValuesBuffer = Buffer.from(decryptedByteValues);
+  let assetIdHex = decryptedByteValuesBuffer.subarray(0, 32).toString("hex");
+  let assetBlind = decryptedByteValuesBuffer.subarray(32, 64).toString("hex");
+
+  let secidx: any[] = [];
+  for (let i = 0; i < 26; i++) {
+    secidx[i] = Number(valueBigIntArg >> BigInt(i * 2)) & 3;
+  }
+
+  let parsedCommitments: any[] = [];
+
+  for (let i = 0; i < 26; i++) {
+    parsedCommitments.push(commitments.subarray(i * 32, i * 32 + 32));
+  }
+
+  let extraCommitBuffer = copyBytes(t.outs[index].script);
+
+  let { k, es, sec } = generateRangeProof2(
+    serializedPoint,
+    serializedGenP,
+    nonce,
+    valueBigIntArg,
+    extraCommitBuffer,
+    assetIdHex,
+    assetBlind,
+    sharedRootMessageHash,
+    genP
+  );
+
+  const lastRing = 25;
+  let ind = secidx[lastRing];
+  let realSigForLastRing = signatures.subarray(
+    lastRing * 4 * 32 + ind * 32,
+    lastRing * 4 * 32 + ind * 32 + 32
+  );
+
+  let e_i_inverse: bigint = invert(es[es.length - 1], Fn.ORDER);
+  let base: bigint = Fn.create(
+    Fn.create(Fn.fromBytes(k[lastRing]) - Fn.fromBytes(realSigForLastRing)) *
+      e_i_inverse
+  );
+  let ephemeralOutputBlind = Fn.create(base - Fn.fromBytes(sec[lastRing]));
+
+  let { finalProof: rangeProof } = generateRangeProof(
+    serializedPoint,
+    serializedGenP,
+    ephemeralOutputBlind,
+    nonce,
+    valueBigIntArg,
+    extraCommitBuffer,
+    assetIdHex,
+    assetBlind,
+    genP
+  );
+
+  console.log();
+
+  // //t.outs[index].rangeProof!.subarray(846)
+  t.outs[index].rangeProof! = Buffer.from(rangeProof);
+  console.log("\n\nTXHEX2\n\n", t.toHex());
+
+  return t;
 }
 
-let decryptedByteValuesBuffer = Buffer.from(decryptedByteValues);
-
-console.log("DECRYPTED", decryptedByteValuesBuffer.toString("hex"));
-
-// ADDED
-// decryptedByteValuesBuffer[64] = 0x6a
-// let decryptedByteValues2: number[] = [];
-// for(let i = 0 ; i < 3328; i++) {
-//   let byteNum = i;
-//   let row = Math.floor(byteNum/(32));
-//   let index = byteNum % 32;
-//   decryptedByteValues2.push(signatures[i]  ^ decryptionKeys[row][index])
-// }
-//END_ADDED
-
-// process.exit()
-
-let valueBigIntArg: bigint = BigInt(
-  "0x" +
-    Buffer.from(
-      decryptedByteValues.slice(decryptedByteValues.length - 8)
-    ).toString("hex")
-); //BigInt(0x0000000005f5e0ff);
-
-//001466f05bc559d7e0d8471c703089d79e582fdd731b
-// let extraCommitBuffer = Buffer.from(
-//   "0014caea90c0357c99bbf305c7971a2b7e1218a641fd",
-//   "hex"
-// ) as Uint8Array;
-
-let extraCommitBuffer = copyBytes(t.outs[index].script);
-
-let assetIdHex = decryptedByteValuesBuffer.subarray(0, 32).toString("hex");
-let assetBlind = decryptedByteValuesBuffer.subarray(32, 64).toString("hex");
-
-// let bb = Fn.toBytes(Fn.create(blindingKey));
-
-// let b_orig =
-//   0xabf582994e1518e46672669d44af3a522fd3c880e094f06fc12a7228549adab4n;
-
-// WHY IS THIS DIFFERENT FROM BLINDING KEY
-// Because blind for output is diff from blinding key
-// We need a specific blind value so that the sum of blinds equals sum of everything else (both blind and unblind)
-let bx = 0xab8b1b80a73864094b435de7ee3a1f52ffea6957fb4bdc2d7b3b9f22eb2a5e35n;
-// let by =
-//   Fp.create(
-//     0xab8b1b80a73864094b435de7ee3a1f52ffea6957fb4bdc2d7b3b9f22eb2a5e35n
-//   );
-
-//////
-
-let secidx: any[] = [];
-
-for (let i = 0; i < 26; i++) {
-  secidx[i] = Number(valueBigIntArg >> BigInt(i * 2)) & 3;
-}
-
-// last commitment is ommitted from range proof
-let secondToLastCommitmentIndex = 24;
-let secondToLastCommitment = commitments.subarray(
-  secondToLastCommitmentIndex * 32,
-  secondToLastCommitmentIndex * 32 + 32
-);
-let vValue0 = BigInt(
-  BigInt(secidx[secondToLastCommitmentIndex]) <<
-    (BigInt(secondToLastCommitmentIndex) * 2n)
-);
-let C = G.multiply(Fn.fromBytes(secondToLastCommitment));
-
-// C is just bG because sec[24] is 0 with 1 btc
-// let negValPoint = genP.multiply(Fn.create(vValue0)).negate()
-// let baseC = C.add(negValPoint)
-
-// let vValue1 = BigInt(BigInt(secidx[secondToLastCommitmentIndex]) << (BigInt(secondToLastCommitmentIndex) * 2n));
-// let posValPoint = genP.multiply(Fn.create(vValue1))
-// let lastC = C.add(posValPoint);
-
-let parsedCommitments: any[] = [];
-
-for (let i = 0; i < 26; i++) {
-  parsedCommitments.push(commitments.subarray(i * 32, i * 32 + 32));
-}
-
-// let lastCommitment = commitments.subarray(
-//   commitments.length - 32,
-//   commitments.length
-// );
-
-//valueBigIntArg
-
-//decryptedByteValuesBuffer.subarray(14, 814)
-//decryptedByteValuesBuffer.subarray(14, 814).subarray(0, 32)
-let { k, es, sec } = generateRangeProof2(
-  serializedPoint,
-  serializedGenP,
-  nonce,
-  valueBigIntArg,
-  extraCommitBuffer,
-  assetIdHex,
-  assetBlind,
-  sharedRootMessageHash,
-  genP
-);
-
-/*
-    (k - s)/e = sec
-
-    s - k/-e = sec
-
-    s - k = -e*sec
-    - s + k = e*sec
-    -s = e*sec - k
-
-
-
-  */
-
-const lastRing = 25;
-let ind = secidx[lastRing];
-let realSigForLastRing = signatures.subarray(
-  lastRing * 4 * 32 + ind * 32,
-  lastRing * 4 * 32 + ind * 32 + 32
-);
-
-let ss =
-  88095228211852605318268035576450732754294607372745903720937769387000122991356n;
-//this is incorrect without last pubkey
-let e_i_inverse: bigint = invert(es[es.length - 1], Fn.ORDER);
-let base: bigint = Fn.create(
-  Fn.create(Fn.fromBytes(k[lastRing]) - Fn.fromBytes(realSigForLastRing)) *
-    e_i_inverse
-);
-let maybeBlind = Fn.create(base - Fn.fromBytes(sec[lastRing]));
-//Buffer.from(utils_1.Fp.toBytes(gg)).toString("hex")
-//Buffer.from(maybeBlind).toString("hex")
-
-let { finalProof: rangeProof } = generateRangeProof(
-  serializedPoint,
-  serializedGenP,
-  maybeBlind,
-  nonce,
-  valueBigIntArg,
-  extraCommitBuffer,
-  assetIdHex,
-  assetBlind,
-  genP
-);
-
-console.log();
-
-// //t.outs[index].rangeProof!.subarray(846)
-t.outs[index].rangeProof! = Buffer.from(rangeProof);
-console.log("\n\nTXHEX2\n\n", t.toHex());
-
-// fs =""
-// for(let i = 8 ; i < 16; i++) {
-//   let byteNum = 846 + 3320 + i;
-//   let row = Math.floor(byteNum/(32*4));
-//   let index = Math.floor(byteNum / 32);
-//   fs+=(signatures[i]  ^ decryptionKeys[row - 1][index - 1]).toString(16)
-// }
-
-// console.log("\n\n", fs);
+main()
