230fa6d86be0dcc4554c5e6a40fb4d8287b13c8a
Randall Naar <rnd.naar@gmail.com>
2025-10-23 06:57:26 -0400

    Small cleanup of index.ts

--- index.ts
+++ index.ts
@@ -1,78 +1,17 @@
-import { secp256k1 } from "@noble/curves/secp256k1.js";
-
 import {
-  asciiToBytes,
   concatBytes,
   numberToBytesBE,
-  randomBytes,
 } from "@noble/curves/utils.js";
 import { sha256 } from "@noble/hashes/sha2";
 import {
   G,
-  hasEven,
   toBytes,
   Fn,
   Fp,
   generatePublicKeySignature,
-  signPartOne as signFirstRoundForRing,
-  generatePublicKeysForRing,
-  lift_x,
-  invert2
+  signPartOne as signFirstRoundForRing
 } from "./utils";
-import { WeierstrassPoint } from "@noble/curves/abstract/weierstrass";
 import { CurvePoint } from "@noble/curves/abstract/curve";
-
-// let ringPubkeyCollection: Uint8Array[][] = Array(NUMBER_OF_RINGS)
-//   .fill(undefined)
-//   .map(() =>
-//     generatePublicKeysForRing(RING_SIZE, signerIndex, signerPublicKey)
-//   );
-
-function getArrayWithRandomByteValues(size: number): Uint8Array[] {
-  return Array(size)
-    .fill(undefined)
-    .map((_) => randomBytes(32));
-}
-
-const NUMBER_OF_RINGS = 2;
-
-let rsizes = Array(26).fill(4);
-
-let s = Array(NUMBER_OF_RINGS)
-  .fill(undefined)
-  .map((_, index) => getArrayWithRandomByteValues(rsizes[index]));
-
-let sec = Array(NUMBER_OF_RINGS)
-  .fill(undefined)
-  .map((_) => randomBytes(32));
-
-// For range proof, secidx is determined by secidx[i] = (*v >> (i*2)) & 3;
-let secidx = Array(NUMBER_OF_RINGS)
-  .fill(undefined)
-  .map((_, index) => {
-    let randomBytesForSecretIndex = randomBytes(32);
-    const dataView = new DataView(randomBytesForSecretIndex.buffer);
-    return dataView.getUint32(0, false) % rsizes[index];
-  });
-
-let pubs: Uint8Array[][] = Array(NUMBER_OF_RINGS)
-  .fill(undefined)
-  .map((_val, index) =>
-    generatePublicKeysForRing(
-      rsizes[index],
-      secidx[index],
-      secp256k1.getPublicKey(sec[index])
-    )
-  );
-
-let k = Array(NUMBER_OF_RINGS)
-  .fill(undefined)
-  .map((_) => randomBytes(32));
-
-//secp256k1_borromean_sign([], ringPubkeyCollection,
-//Return e0e0,
-
-// secp256k1_borromean_sign(s, pubs, k, sec, secidx, NUMBER_OF_RINGS, message);
 
 export function secp256k1_borromean_sign(
   s: Uint8Array[][],
@@ -89,10 +28,6 @@ export function secp256k1_borromean_sign(
 
   for (let ringIndex = 0; ringIndex < nrings; ringIndex++) {
     let signerNoncePoint = G.multiply(Fp.fromBytes(k[ringIndex]));
-
-    //  if(ringIndex === 25) {
-    //   debugger;
-    //  }
 
     let pubkeys = pubs[ringIndex];
     let { lastRingNonce } = signFirstRoundForRing(
@@ -157,19 +92,6 @@ export function secp256k1_borromean_sign(
     }
 
     es.push(e_i);
-
-    /* sig/sec[ringIndex]
-    s = k - e*sec
-   (s - k)/-e = sec
-   (k - s)/e = sec
-    */
-
-    // Overwrite fake signature in the signer index with a real signature from the signer
-    // Fn for private key
-
-    // if (ringIndex === 25 || ringIndex === 1) {
-    //     debugger;
-    //   }
     if (!skipSig) {
 
       let signerSignature =
@@ -236,4 +158,3 @@ export function secp256k1_borromean_sign2(
 
   return { sharedRootMessageHash: Fn.fromBytes(sharedRootMessageHash), es };
 }
-console.log();
